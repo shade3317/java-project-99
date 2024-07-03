263f741d166d172c7451082ce7d43f8293b9b6d9
Admin <shade641114@gmail.com>
2024-07-03 23:48:00 +0300

    Step_1 1.4

--- src/main/java/hexlet/code/AppApplication.java
+++ src/main/java/hexlet/code/AppApplication.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AppApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(AppApplication.class, args);
-	}
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class, args);
+    }
 
 }
--- src/main/java/hexlet/code/controller/WelcomeController.java
+++ src/main/java/hexlet/code/controller/WelcomeController.java
@@ -3,6 +3,7 @@ package hexlet.code.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class WelcomeController {
     @GetMapping(path = "/welcome")
