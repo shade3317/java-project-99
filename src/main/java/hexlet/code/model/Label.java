6f58a497136b465f5c29b16918b881fed5270806
Admin <shade641114@gmail.com>
2024-07-10 06:13:10 +0300

    Comment_edits 1.9.10

--- src/main/java/hexlet/code/model/Label.java
+++ src/main/java/hexlet/code/model/Label.java
@@ -1,17 +1,13 @@
 package hexlet.code.model;
 
 import java.time.LocalDate;
-//import java.util.Set;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-//import jakarta.persistence.ManyToMany;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.GeneratedValue;
-//import jakarta.persistence.FetchType;
-//import jakarta.persistence.CascadeType;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
@@ -36,7 +32,4 @@ public class Label implements BaseEntity {
 
     @CreatedDate
     private LocalDate createdAt;
-
-    //@ManyToMany(fetch = FetchType.LAZY, mappedBy = "labels", cascade = CascadeType.MERGE)
-    //private Set<Task> tasks;
 }
